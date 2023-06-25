b413548dd37b5eb4b588c2e33f79cd208d71fe49
Cassio Damacena <cassiodamacena@hotmail.com>
2023-06-24 23:58:14 -0300

    Teste quality Gate Sonar

--- src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -15,4 +15,65 @@ import java.time.LocalDate;
 
 public class TaskControllerTest {
 
+    @Mock
+    private TaskRepo taskRepo;
+
+    @InjectMocks
+    private TaskController controller;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemDescricao() {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.now());
+
+        try {
+            controller.save(todo);
+            Assert.fail("Não deveria chegar neste ponto!!!");
+        } catch (ValidationException e) {
+            Assert.assertEquals("Fill the task description", e.getMessage() );
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemData(){
+        Task todo = new Task();
+        todo.setTask("Todo teste");
+
+        try {
+            controller.save(todo);
+            Assert.fail("Não deveria chegar neste ponto!!!");
+        } catch (ValidationException e) {
+            Assert.assertEquals("Fill the due date", e.getMessage() );
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaComDataPassada(){
+        Task todo = new Task();
+        todo.setTask("Todo teste");
+        todo.setDueDate(LocalDate.now().minusDays(1));
+
+        try {
+            controller.save(todo);
+            Assert.fail("Não deveria chegar neste ponto!!!");
+        } catch (ValidationException e) {
+            Assert.assertEquals("Due date must not be in past", e.getMessage() );
+        }
+    }
+
+    @Test
+    public void deveSalvarTarefaComSucesso() throws ValidationException {
+        Task todo = new Task();
+        todo.setTask("Todo teste");
+        todo.setDueDate(LocalDate.now());
+
+        controller.save(todo);
+        Mockito.verify(taskRepo).save(todo);
+    }
+
 }
